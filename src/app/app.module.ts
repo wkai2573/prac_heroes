fc349922f6589921ecfe55b038d4a2480f25a178
wkai <wkai2573@gmail.com>
2021-11-22 16:30:45 +0800

    https://angular.tw/tutorial/toh-pt1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+//angular組件
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; //使[(NgModel)]可用
 
+//自訂組件
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+		FormsModule, //使[(NgModel)]可用
   ],
   providers: [],
   bootstrap: [AppComponent]
